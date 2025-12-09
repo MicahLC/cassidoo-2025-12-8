a4efc9db7b5b5a5d80e7662a24823c49385e228d
Micah Lapping-Carr <MicahLC@users.noreply.github.com>
2025-12-09 17:57:27 -0600

    Remove unnecessary usings

--- Deck.cs
+++ Deck.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace cassidoo_2025_12_8
+﻿namespace cassidoo_2025_12_8
 {
 	public class Deck
 	{
